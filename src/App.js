2e6e789b4b55cb5921e1cf6da0b4cff46d803681
Ilia Sidash <iliasidash@gmail.com>
2018-02-27 12:33:37 +0500

    Fix: [Main] Main was added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Helmet } from "react-helmet";
 
-import Header from "./Header";
-import Directions from "./Directions";
-import Prices from "./Prices";
-import Partners from "./Partners";
-import Subscribe from "./Subscribe";
-import Offer from "./Offer";
-import Help from "./Help";
-import MobileApp from "./MobileApp";
-import Footer from "./Footer";
-
+import Main from "./Main";
 import Search from "./Search";
-
-const Main = () => (
-  <div>
-    <Helmet>
-      <title>Aviasales / Главная страница</title>
-    </Helmet>
-    <Header />
-    <Directions />
-    <Prices />
-    <Partners />
-    <Subscribe />
-    <Offer />
-    <Help />
-    <MobileApp />
-  </div>
-);
+import Footer from "./Footer";
 
 export default () => {
   return (
---
+++ src/Main/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Helmet } from "react-helmet";
+
+import Header from "../Header";
+import Directions from "../Directions";
+import Prices from "../Prices";
+import Partners from "../Partners";
+import Subscribe from "../Subscribe";
+import Offer from "../Offer";
+import Help from "../Help";
+import MobileApp from "../MobileApp";
+
+const Main = styled.div``;
+
+export default () => (
+  <Main>
+    <Helmet>
+      <title>Aviasales / Главная страница</title>
+    </Helmet>
+    <Header />
+    <Directions />
+    <Prices />
+    <Partners />
+    <Subscribe />
+    <Offer />
+    <Help />
+    <MobileApp />
+  </Main>
+);
