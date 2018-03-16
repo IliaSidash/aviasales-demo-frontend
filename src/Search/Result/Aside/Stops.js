0e519b4d98aaccbaeb1991e33eaf4b451e6bdf13
Ilia Sidash <iliasidash@gmail.com>
2018-03-16 11:25:29 +0500

    Fix: Stops was edited

--- src/Search/Result/Aside/Stops.js
+++ src/Search/Result/Aside/Stops.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Checkbox from './Checkbox';
 
@@ -10,21 +11,31 @@ const stopText = {
 };
 
 const Stops = (props) => {
-  const { checkboxes, checkedAll } = props.stops;
-  const { onChangeAll, onChange, component } = props;
+  const { checkboxes } = props.stops;
+  const { checkedAll, onChangeAll, onChange } = props;
+
   return (
     <React.Fragment>
-      <Checkbox text="Все" checked={checkedAll} onChange={() => onChangeAll(component)} />
+      <Checkbox text="Все" checked={checkedAll} onChange={() => onChangeAll('stops')} />
 
       {checkboxes.map((checkbox, index) => (
         <Checkbox
           text={stopText[checkbox.stops]}
           checked={checkbox.checked}
-          onChange={() => onChange(component, index)}
+          onChange={() => onChange('stops', index)}
         />
       ))}
     </React.Fragment>
   );
+};
+
+Stops.propTypes = {
+  stops: PropTypes.arrayOf(PropTypes.shape({
+    checkboxes: PropTypes.array,
+  })).isRequired,
+  checkedAll: PropTypes.bool.isRequired,
+  onChangeAll: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default Stops;
